aa52d10039d5f1724496b13a8da0bc2324637e9c
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-11-28 18:22:06 +0600

    added route to get all subjects marks for a student with subject and semester filtering.The returned result will be divided by semester

--- app/routes/mark_routes.py
+++ app/routes/mark_routes.py
@@ -1,7 +1,7 @@
 from app.schemas.marks_schema import MarksCreateSchema, MarksResponseSchema, MarksUpdateSchema
 from app.schemas.user_schema import UserOutSchema
 from app.services.marks_service import MarksService
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.db import get_db_session
 from app.permissions.role_checks import ensure_admin_or_teacher, ensure_admin
@@ -41,3 +41,27 @@ async def update_a_mark(
 ):
 
     return await MarksService.update_mark(db, mark_data, mark_id, current_user)
+
+
+# delete marks
+@router.delete("/{mark_id}",
+    dependencies=[Depends(ensure_admin)],
+    response_model=MarksResponseSchema
+)
+async def delete_a_mark(
+    mark_id: int,
+    db: AsyncSession = Depends(get_db_session),
+):
+    return await MarksService.delete_mark(db, mark_id)
+
+
+# get all subjects marks for a student
+@router.get("/student/{student_id}",)
+async def get_all_subjects_marks_for_a_student(
+    student_id: int,
+    semester_id: int | None = Query(None),
+    subject_id: int | None = Query(None),
+    db: AsyncSession = Depends(get_db_session),
+    current_user: UserOutSchema = Depends(get_current_user),
+):
+    return await MarksService.get_all_marks_for_a_student(db, student_id, semester_id, subject_id)
