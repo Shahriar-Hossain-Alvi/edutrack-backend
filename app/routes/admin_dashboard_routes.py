20e04b81b95290a4336e4571ee6337154c3271fe
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2026-05-07 20:29:23 +0600

    created api to get pie chart data to show number of student and teacher in every department

--- app/routes/admin_dashboard_routes.py
+++ app/routes/admin_dashboard_routes.py
@@ -40,3 +40,39 @@ async def get_all_table_data_count_stats(
             }
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+
+
+# Department-wise Distribution: কোন ডিপার্টমেন্টে কতজন ছাত্র বা শিক্ষক আছে তার একটি Pie Chart।
+@router.get("/pieChart")
+async def get_chartsData(
+    request: Request,
+    db: AsyncSession = Depends(get_db_session),
+    authorized_user: UserOutSchema = Depends(
+        ensure_roles(["super_admin", "admin"])),
+):
+    try:
+        return await AdminDashboardService.get_chart_data(db, request)
+    except DomainIntegrityError as de:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=de.error_message
+        )
+    except HTTPException:
+        raise
+    except Exception as e:
+        logger.critical(f"Unexpected Error: {e}")
+        # attach audit payload
+        if request:
+            request.state.audit_payload = {
+                "raw_error": str(e),
+                "exception_type": type(e).__name__,
+            }
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+
+# Result Performance: বিভিন্ন সেমিস্টারের গড় জিপিএ(Average GPA) এর একটি Bar Chart। এটি থেকে বোঝা যাবে কোন সেমিস্টারের রেজাল্ট কেমন হচ্ছে।
+
+# 2. Audit Log Monitoring
+# Showing Recent Activity: "Recent Activity" -> last 10 actions
+# Filter by Level: শুধুমাত্র CRITICAL বা ERROR লেভেলের লগগুলো আলাদা করে দেখানোর জন্য একটি টগল বা বাটন।
+# Database Load: আপনার অডিট টেবিল কত দ্রুত বড় হচ্ছে বা মোট কতগুলো এন্ট্রি আছে তার একটি স্ট্যাটাস।
--- app/services/admin_dashboard_service.py
+++ app/services/admin_dashboard_service.py
@@ -8,6 +8,7 @@ from app.models import User
 from app.models import Department, Mark, Semester, Student, Subject, SubjectOfferings, Teacher
 from sqlalchemy.exc import IntegrityError
 from fastapi import Request
+import re
 
 
 class AdminDashboardService:
@@ -61,41 +62,6 @@ class AdminDashboardService:
                 "assigned courses": row.assigned_courses,
                 "marks": row.marks
             }
-
-        # helper function to get count of any model/table
-        # async def get_count(model):
-        #     query = select(func.count(model.id))
-        #     result = await db.execute(query)
-        #     return result.scalar() or 0
-
-        # # Count all users
-        # total_users = await get_count(User)
-
-        # # Count total admin
-        # admin_count_result = await db.execute(select(func.count(User.id)).where(
-        #     or_(
-        #         User.role == "admin",
-        #         User.role == "super_admin"
-        #     )
-        # ))
-
-        # total_admins = admin_count_result.scalar() or 0
-
-        # # direct counts from other tables
-        # counts = {
-        #     "users": total_users,
-        #     "admins": total_admins,
-        #     "teachers": await get_count(Teacher),
-        #     "students": await get_count(Student),
-        #     "departments": await get_count(Department),
-        #     "semesters": await get_count(Semester),
-        #     "subjects": await get_count(Subject),
-        #     "assigned courses": await get_count(SubjectOfferings),
-        #     "marks": await get_count(Mark)
-        # }
-
-        # return counts
-
         except IntegrityError as e:
             # Important: rollback as soon as an error occurs. It recovers the session from 'failed' state and puts it back in 'clean' state to save the Audit Log
             await db.rollback()
@@ -120,40 +86,82 @@ class AdminDashboardService:
                 error_message=readable_error, raw_error=raw_error_message
             )
 
+    @staticmethod
+    def format_chart_data(pie_chart_data):
+        formatted_labels = []
 
-# @staticmethod
-# async def get_all_table_data_count(db: AsyncSession, request: Request | None = None):
-#     try:
-#         # We fetch all counts in one single SQL execution
-#         query = select(
-#             select(func.count(User.id)).scalar_subquery().label("users"),
-#             select(func.count(User.id)).where(User.role.in_(
-#                 ["admin", "super_admin"])).scalar_subquery().label("admins"),
-#             select(func.count(Teacher.id)).scalar_subquery().label("teachers"),
-#             select(func.count(Student.id)).scalar_subquery().label("students"),
-#             select(func.count(Department.id)
-#                    ).scalar_subquery().label("departments"),
-#             select(func.count(Semester.id)).scalar_subquery().label("semesters"),
-#             select(func.count(Subject.id)).scalar_subquery().label("subjects"),
-#             select(func.count(SubjectOfferings.id)
-#                    ).scalar_subquery().label("assigned_courses"),
-#             select(func.count(Mark.id)).scalar_subquery().label("marks")
-#         )
+        for item in pie_chart_data:
+            raw_name = item["department_name"]
+            parts = re.split(r'[-–—:]', raw_name)
 
-#         result = await db.execute(query)
-#         row = result.fetchone()
+            abbr = parts[0].strip().upper()
+            formatted_labels.append(abbr)
 
-#         return {
-#             "users": row[0],
-#             "admins": row[1],
-#             "teachers": row[2],
-#             "students": row[3],
-#             "departments": row[4],
-#             "semesters": row[5],
-#             "subjects": row[6],
-#             "assigned courses": row[7],
-#             "marks": row[8]
-#         }
-#     except Exception as e:
-#         await db.rollback()
-        # ... your existing error handling ...
+        return {
+            "labels": formatted_labels,
+            "values": [item["count"] for item in pie_chart_data]
+        }
+
+    @staticmethod
+    async def get_chart_data(
+        db: AsyncSession,
+        request: Request | None = None
+    ):
+
+        try:
+            # Get department names with count of students
+            query = select(
+                Department.department_name,
+                func.count(Student.id)).join(Student, Department.id == Student.department_id).group_by(Department.department_name)
+
+            # Execute the query
+            result = await db.execute(query)
+
+            pie_chart_data = result.mappings().all()
+
+            # group departments in an array and counts in another array
+            student_pie_chart_data = AdminDashboardService.format_chart_data(
+                pie_chart_data)
+
+            # Get department names with count of students
+            query = select(
+                Department.department_name,
+                func.count(Teacher.id)).join(Teacher, Department.id == Teacher.department_id).group_by(Department.department_name)
+
+            # Execute the query
+            result = await db.execute(query)
+
+            pie_chart_data = result.mappings().all()
+
+            # group departments in an array and counts in another array
+            teacher_pie_chart_data = AdminDashboardService.format_chart_data(
+                pie_chart_data)
+
+            return {
+                "student_pie_chart_data": student_pie_chart_data,
+                "teacher_pie_chart_data": teacher_pie_chart_data
+            }
+
+        except IntegrityError as e:
+            # Important: rollback as soon as an error occurs. It recovers the session from 'failed' state and puts it back in 'clean' state to save the Audit Log
+            await db.rollback()
+
+            # generally the PostgreSQL's error message will be in e.orig.args
+            raw_error_message = str(e.orig) if e.orig else str(e)
+            readable_error = parse_integrity_error(raw_error_message)
+
+            logger.error(f"Integrity error while getting all table count: {e}")
+            logger.error(f"Readable Error: {readable_error}")
+
+            # attach audit payload safely
+            if request:
+                payload: dict[str, Any] = {
+                    "raw_error": raw_error_message,
+                    "readable_error": readable_error,
+                }
+
+                request.state.audit_payload = payload
+
+            raise DomainIntegrityError(
+                error_message=readable_error, raw_error=raw_error_message
+            )
